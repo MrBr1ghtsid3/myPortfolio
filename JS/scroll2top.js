587a3804bcd5793f00814ff758efe15993409b60
MrBr1ghtsid3 <tsvetoslav.shalev@gmail.com>
2021-02-20 13:56:56 +0000

    Resolve Scroll2Top

--- JS/scroll2top.js
+++ JS/scroll2top.js
@@ -1,14 +1,17 @@
 // Scroll 2 Top
 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 25) {        // If page is scrolled more than 50px
+    if ($(this).scrollTop() >= 350) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+
+// Muted while I figure href=javascript: linking
+
+// $('#return-to-top').click(function(clickMe) {      // When arrow is clicked
+//     $('body,html').animate({
+//         scrollTop : 0                       // Scroll to top of body
+//     }, 500);
+// });
